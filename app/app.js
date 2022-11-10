6b27cc1e0d974cdd527bbcd483bfb584a2f53996
bss0727 <tjqjatjr1212@gmail.com>
2022-11-11 01:49:14 +0900

    Backend에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 "use strict"
 
+// module
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const PORT = 3000;
@@ -9,6 +11,8 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", home);
 
