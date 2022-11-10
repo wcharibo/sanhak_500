57bb9da54cbb2ca2fa14b4dc2dec21227b23ff7c
bss0727 <tjqjatjr1212@gmail.com>
2022-11-11 01:12:34 +0900

    로그인 요청 fetch생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,5 +11,12 @@ function login() {
         id: id.value,
         psword: psword.value,
     };
-    console.log(req);
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "content-Type": "application/json"
+        },
+        body: JSON.stringify(req),
+    });
 }
