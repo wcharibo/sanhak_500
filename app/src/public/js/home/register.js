750792fd4d539d3539e0e66b983522c9c36d0a47
BSS <tjqjatjr1212@gamil.com>
2022-11-12 17:24:27 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,11 +9,12 @@ const id = document.querySelector("#id"), // #: tag에 id로 부여되어 있는
 registerBtn.addEventListener("click", register);
 
 function register() {
+  if (!id.value) return alert("아이디를 입력해주십시오.");
+  if (psword.value !== confirmPsword.value) return alert("비밀번호가 일치하지 않습니다.");
   const req = {
     id: id.value,
     name: name.value,
     psword: psword.value,
-    confirmPsword: confirmPsword.value,
   };
 
   fetch("/register", {
