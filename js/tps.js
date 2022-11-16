b5bd1202e44830d2c3b41a6ff4475c670f6f9115
wcharibo <woonggi4283@gmail.com>
2022-11-16 13:58:34 +0900

    add 'fixed' classList at characterSpan to detect how many wrong word entered

--- js/tps.js
+++ js/tps.js
@@ -3,14 +3,13 @@ const wordDisplayElement = document.querySelector('#wordDisplay');
 const wordInputElement = document.querySelector('#wordInput');
 const timerElement = document.querySelector('#timer');
 const restartElement = document.querySelector('#restartBtn');
-let errorCnt =0;
 
 import {wordData} from "./wordList.js";
 
 function randomWord(){
     let random = Math.floor(Math.random()*(10000-1)+1);
     let arr = wordData[random];
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 1; i++) {
         random = Math.floor(Math.random()*(10000-1)+1);
         arr = arr + ' ' + wordData[random];
     }
@@ -20,7 +19,7 @@ function randomWord(){
 function getPractice() {
     const word = randomWord();
     wordDisplayElement.innerText = '';
-    word.split('').forEach(character =>{
+    word.split('').forEach((character) =>{
         const characterSpan = document.createElement('span');
         characterSpan.innerText = character;
         wordDisplayElement.appendChild(characterSpan);
@@ -55,40 +54,56 @@ wordInputElement.addEventListener('focus',()=>{     //input focus->start timer, 
 
 
 wordInputElement.addEventListener('input', () =>{  //check every input
-const arrayWord = wordDisplayElement.querySelectorAll('span');
-const arrayValue = wordInputElement.value.split('');
-
-let correct = true;
-arrayWord.forEach((characterSpan, index)=>{
-    const character = arrayValue[index];
-    // console.log(character);
-    if(character == null){
-        characterSpan.classList.remove('correct');
-        characterSpan.classList.remove('incorrect');
-        correct = false;
-    }
-    else if(character === characterSpan.innerText){
-        characterSpan.classList.add('correct');
-        characterSpan.classList.remove('incorrect');
-    }
-    else{
-        characterSpan.classList.remove('correct');
-        characterSpan.classList.add('incorrect');
-        correct = false;
-        errorCnt = errorCnt + 1;
-    }
+    const arrayWord = wordDisplayElement.querySelectorAll('span');
+    const arrayValue = wordInputElement.value.split('');
+    
+    let correct = true;
+    let errCnt=0;
+    arrayWord.forEach((characterSpan, index)=>{
+        const character = arrayValue[index];
+        let a=1;
+        wordInputElement.addEventListener('keydown', (event)=>{
+            const key = event.key;
+            if(key === 'Backspace') {a=0; console.log('backspace');}
+        })
+        if(character == null){
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.remove('incorrect');
+            correct = false;
+        }
+        else if(character === characterSpan.innerText & characterSpan.classList=='incorrect'){
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect');
+        }
+        else if(character === characterSpan.innerText){
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect');
+        }
+        else{
+            characterSpan.classList.remove('correct');
+            characterSpan.classList.add('incorrect');
+            characterSpan.classList.add('fixed')
+            correct = false;
+        }
     });
-
-    if(correct) {console.log(errorCnt);}  //renderNewQuote()
-})
-
-restartElement.addEventListener('click', ()=>{  //restart Button
-    getPractice();
-});
-
-let startTime, t;
-function startTimer(i){
-    timerElement.innerText = 0;
+    
+    if(correct) {
+        for (let i = 0; i < arrayWord.length; i++) {
+            if(arrayWord[i].className =='fixed correct'){
+                errCnt += 1;
+            }
+        }
+            console.log(`${errCnt} error detected`);
+        }  //renderNewQuote()
+    })
+    
+    restartElement.addEventListener('click', ()=>{  //restart Button
+        getPractice();
+    });
+    
+    let startTime, t;
+    function startTimer(i){
+        timerElement.innerText = 0;
     if (i==1) {
         startTime = new Date();
     t = setInterval(()=>{
