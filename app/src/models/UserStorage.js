d73b83061aaed1a312bc97277710dcb1c2cccc6d
BSS <tjqjatjr1212@gamil.com>
2022-11-16 16:22:49 +0900

    winston 프로젝트에 적용

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -8,7 +8,7 @@ class UserStorage {
       const query = "SELECT * FROM users WHERE id = ?;";
       db.query(query, [id], (err, data) => {
         if (err) reject(`${err}`);
-        resolve(data[0]);
+        else resolve(data[0]);
       });
     });   
   }
@@ -22,7 +22,7 @@ class UserStorage {
         [userInfo.id, userInfo.name, userInfo.psword], 
         (err) => {
         if (err) reject(`${err}`);
-        resolve({ success: true });
+        else resolve({ success: true });
       });
     });  
   }
