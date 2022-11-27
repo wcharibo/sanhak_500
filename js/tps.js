d954893c6ea2dbc91ebd9252d5017924ac48779e
wcharibo <woonggi4283@gmail.com>
2022-11-27 16:56:31 +0900

    Add TimeTable for check word input time

--- js/tps.js
+++ js/tps.js
@@ -6,28 +6,30 @@ const restartElement = document.querySelector('#restartBtn');
 const resultDisplayElement = document.querySelector('#resultDisplay');
 let errCnt = 0;
 let backspaceCnt = 0;
-let timerSet = 0;
+let timeTable = new Array();
+let timeCnt=0;
 
 import {wordData} from "./wordList.js";
 
 function randomWord(){
     let random = Math.floor(Math.random()*(10000-1)+1);
     let arr = wordData[random];
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 20; i++) {
         random = Math.floor(Math.random()*(10000-1)+1);
         arr = arr + ' ' + wordData[random];
     }
     return arr;
 };
 
+
 function getPractice() {
     const word = randomWord();
     wordDisplayElement.innerText = '';
-    word.split('').forEach((character) =>{
-        const characterSpan = document.createElement('span');
-        characterSpan.innerText = character;
-        wordDisplayElement.appendChild(characterSpan);
-    })
+        word.split('').forEach((character) =>{
+            const characterSpan = document.createElement('span');
+            characterSpan.innerText = character;
+            wordDisplayElement.appendChild(characterSpan);
+        })
     wordInputElement.value = null;
 };
 
@@ -36,7 +38,7 @@ const getResult =()=>{
     resultDisplayElement.style.display='block';
     wordDisplayElement.style.display = 'none';
     wordInputElement.style.display = 'none';
-    let result = `${errCnt} wrong word and ${backspaceCnt} backspace`
+    let result = `${errCnt} wrong word`
     resultDisplayElement.innerHTML= result;
 }
 
@@ -73,55 +75,63 @@ wordInputElement.addEventListener('focus',()=>{     //input focus->start timer, 
 wordInputElement.addEventListener('input', () =>{  //check every input
     const arrayWord = wordDisplayElement.querySelectorAll('span');
     const arrayValue = wordInputElement.value.split('');
-    console.log(arrayValue.length);//이게 키포인트 인듯
-    console.log(arrayWord[arrayValue.length-1]);
+    let key;
+    // console.log(arrayValue);//이게 키포인트 인듯
+    // console.log(arrayWord[arrayValue.length-1].innerText);
     let correct = true;
+    timeTable[timeCnt]=new Date();
     arrayWord.forEach((characterSpan, index)=>{
         const character = arrayValue[index];
         // wordInputElement.addEventListener('keydown', (event)=>{
-        //     const key = event.key;
-        //     if(key === 'Backspace') {backspaceCnt+=1;}
+        //     key = event.keyCode;
         // })
         if(character == null){
             characterSpan.classList.remove('correct');
             characterSpan.classList.remove('incorrect');
             correct = false;
         }
+        else if(character == ' ' && characterSpan.innerText == ' '){
+            characterSpan.classList.add('space');
+        }
         else if(character === characterSpan.innerText){
             characterSpan.classList.add('correct');
             characterSpan.classList.remove('incorrect');
         }
-        else if(arrayValue.length>=3){
-            if(character != characterSpan.innerText && arrayWord[arrayValue.length-3].className=='fixed incorrect'){
-                characterSpan.classList.add('incorrect');
-            }
-            else if(character != characterSpan.innerText && arrayWord[arrayValue.length-3].className=='incorrect'){
-                characterSpan.classList.add('incorrect');
-            }
-            else if(character != characterSpan.innerText && arrayWord[arrayValue.length-2].className=='correct'){
-                characterSpan.classList.remove('correct');
-                characterSpan.classList.add('incorrect');
-                characterSpan.classList.add('fixed')
-                correct = false;
-            }
-        }
+        // else if(arrayValue.length>=3){
+        //     if(character != characterSpan.innerText && arrayWord[arrayValue.length-3].className=='fixed incorrect'){
+        //         characterSpan.classList.add('incorrect');
+        //     }
+        //     else if(character != characterSpan.innerText && arrayWord[arrayValue.length-3].className=='incorrect'){
+        //         characterSpan.classList.add('incorrect');        
+        //     }
+        //     else if(character != characterSpan.innerText && arrayWord[arrayValue.length-2].className=='correct'){
+        //         characterSpan.classList.remove('correct');
+        //         characterSpan.classList.add('incorrect');
+        //         characterSpan.classList.add('fixed')
+        //         correct = false;
+        //     }
+        // }
         else{
             characterSpan.classList.remove('correct');
             characterSpan.classList.add('incorrect');
             characterSpan.classList.add('fixed')
             correct = false;
+            timeTable[timeCnt]=0;
         }
-    });
+        });
 
-    if(correct) {
+    if(arrayWord.length==arrayValue.length || timerElement.innerText == 0) {
         for (let i = 0; i < arrayWord.length; i++) {
-            if(arrayWord[i].className =='fixed correct'){
+            if(arrayWord[i].className.includes('fixed')){
                 errCnt += 1;
             }
         }   
             getResult();
+            wordTime();
             console.log(`${errCnt} error detected`);
+            console.log(arrayWord);
         }  //renderNewQuote()
+        timeCnt+=1;
     })
     
     restartElement.addEventListener('click', ()=>{  //restart Button
@@ -129,8 +139,14 @@ wordInputElement.addEventListener('input', () =>{  //check every input
         wordDisplayElement.style.display = 'block';
         wordInputElement.style.display = 'block';
         errCnt = 0;
+        timeTable=new Array;
+        timeCnt = 0;
         getPractice();
     });
+
+    function wordTime(){
+        console.log(timeTable);
+    };
     
     let startTime, timerInterval;
     function startTimer(i){ //
